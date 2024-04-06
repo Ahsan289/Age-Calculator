438c37e0f83bc01756d251b1450acc677ad65699
Ahsan Tariq <166172700+Ahsan289@users.noreply.github.com>
2024-04-06 23:25:57 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,81 @@
+let a = +prompt("Enter 1st Number : ");
+let b = +prompt("Enter 2nd Number : ");
+//arithematic operators
+
+
+
+let addition = a + b;
+let subtraction = a - b;
+let multiplication = a * b;
+let division = a / b;
+let modulus = a % b;
+
+
+
+// Increment and Decrement Operators
+
+
+let increment = ++a;
+let increment2 = ++b;
+let decrement = --a;
+let decrement2 = --b;
+document.write("<br> 1st Number is : " + a + "<br> 2nd Number is : " + b + "<br> Addition is : " + addition + "<br> Subtraction is : " + subtraction + "<br> Multiplication is : " + multiplication + "<br> Division is : " + division + "<br> Modulus Is : " + modulus + "<br> Increment of 1st Number is : " + increment + "<br> Increment of 2nd Number is : " + increment2 + "<br> Decrement of 1st Number is : " + decrement + "<br> Decrement of 2nd Number is : " + decrement2);
+
+
+//assignment operators
+
+
+
+let c = 1;
+c+=1;
+document.write("<br> c = 1 " + "<br> c += 1 " + "<br> c = " + c);
+let d = 2;
+d-=1;
+document.write("<br> d = 2 " + "<br> d -= 1 " + "<br> d = " + d);
+let e = 3;
+e *= 4;
+document.write("<br> e = 3 " + "<br> e *= 4 " + "<br> e = " + e);
+let f = 8;
+f /= 4;
+document.write("<br> f = 8 " + "<br> f /= 4 " + "<br> f = " + f);
+let g = 5;
+g **= 5;
+document.write("<br> g = 5 " + "<br> g ** 5 " + "<br> g = " + g);
+
+
+
+// comparison operators
+
+
+
+let num1 = +prompt("Enter Num 1 for comparison : ");
+let num2 = +prompt("Enter Num 2 for comparison : ");
+if (num1==num2) {
+    document.write("<br> True " + num1 + " is equal " + num2 );
+    
+} else {
+    document.write("<br> False " + num1 +  " is not equal to " + num2);
+}
+
+let num3 = +prompt("Enter Num 3 for comparison : ");
+let num4 = +prompt("Enter Num 4 for comparison : ");
+if (num3!=num4){
+    document.write("<br> True " + num3 + " is not equal to " + num4);
+}
+else{
+    document.write("<br> False " + num3 + " is equal to " + num4);
+}
+
+let num5 = +prompt("Enter Num 5 for comparison : ");
+let num6 = +prompt("Enter Num 6 for comparison : ");
+if ()
+
+
+
+
+
+
+
+
+
+
